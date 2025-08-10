65cf3738167b6df5ece49c42b9ca94f0409468c3
Saniya S Inamdar <39870781+CodeFor2001@users.noreply.github.com>
2025-08-10 19:27:00 +0100

    Add Recognition Section to Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Bio from '../components/Bio';
+import Recognition from '../components/Recognition';
 import Skills from '../components/Skills';
 import Contact from '../components/Contact';
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div className="pt-16">
       <Hero />
       <Bio />
+      <Recognition />
       <Skills />
       <Contact />
     </div>
